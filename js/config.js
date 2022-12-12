ec96a17de2b2adeb100af2df126a15a3d4bcb24f
Lillebror420 <mikael@tommelop.dk>
2022-12-12 19:39:49 +0100

    Config update hopefull it works

--- js/config.js
+++ js/config.js
@@ -1,4 +1,17 @@
-const firstBtn = document.getElementById("box1")
+const firstBtn = document.getElementById("firstBtn")
+const secondBtn = document.getElementById("secondBtn")
+const thirdBtn = document.getElementById("thirdBtn")
 
+const miniFirstButton = document.getElementById("miniFirstButton")
+const miniSecondButton = document.getElementById("miniSecondButton")
 
-firstBtn.innerHTML = "Discord"
+// Here you can change the content of the buttons
+
+// Here are the first three buttons
+firstBtn.innerHTML = "Singleplayerr"
+secondBtn.innerHTML = "Multiplayer"
+thirdBtn.innerHTML = "Minecraft Realms"
+
+// Here are the two small buttons
+miniFirstButton.innerHTML = "Options..."
+miniSecondButton.innerHTML = "Quit Game"
